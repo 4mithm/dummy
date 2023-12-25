402850801aab8bbe184f479ae47dabe5ef2a21c0
4mithm <90064206+4mithm@users.noreply.github.com>
2023-12-25 12:40:03 +0530

    Delete p1.py

--- p1.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-
-sum = num1 + num2
-
-print("The sum is:", sum)
-
-print("this is for checking the pull request ")
